a0647806dadf0bcb8d86a3dc86cd4196013f3f6f
edwardlin1991 <eddy@Edwards-MacBook-Pro.local>
2017-03-04 16:15:39 -0600

    Create Lamar Corridor Lsit
    A order list of sensors on Lamar

--- create_corridor.py
+++ create_corridor.py
@@ -1,25 +1,26 @@
 from database_setup import Base, User, TravelSensor, Summary, db_session
 import pickle
+import math
+from create_map import Map
 
-class Map:
+class Corridor(Map):
 	READER_IDs = []
 	ADJ_INTERSECTIONS = {}
 
 	def get_map_from_database(self):
-		travelSensors = db_session.query(TravelSensor).all()
+		travelSensors = db_session.query(TravelSensor).filter(TravelSensor.READER_ID.contains('lamar')).order_by(TravelSensor.LATITUDE.desc())
 		self.READER_IDs = [x.READER_ID for x in travelSensors]
 		for sensor in self.READER_IDs:
-			self.ADJ_INTERSECTIONS[sensor] = db_session.query(Summary.DESTINATION).filter(Summary.ORIGIN==sensor).distinct(Summary.DESTINATION).all()
 			print sensor
-			print self.ADJ_INTERSECTIONS[sensor]
 
 def get_map_from_file():
 	return pickle.load('map.dump')
 
 
 if __name__ == '__main__':
-	m = Map()
+	m = Corridor()
 	m.get_map_from_database()
+	# m.create_corridor()
 
 	output = open('map.dump', 'wb')
 	pickle.dump(m, output)
