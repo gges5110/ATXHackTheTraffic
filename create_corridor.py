6dc63f8bd9d0a113f5aab036941e94b60ba8611a
edwardlin1991 <eddy@Edwards-MacBook-Pro.local>
2017-03-04 16:30:04 -0600

    create-corridor

---
+++ create_corridor.py
@@ -0,0 +1,28 @@
+from database_setup import Base, User, TravelSensor, Summary, db_session
+import pickle
+import math
+from create_map import Map
+
+class Corridor(Map):
+	READER_IDs = []
+	ADJ_INTERSECTIONS = {}
+
+	def get_map_from_database(self):
+		travelSensors = db_session.query(TravelSensor).filter(TravelSensor.READER_ID.contains('lamar')).order_by(TravelSensor.LATITUDE.desc())
+		self.READER_IDs = [x.READER_ID for x in travelSensors]
+		for sensor in self.READER_IDs:
+			print sensor
+
+def get_map_from_file():
+	return pickle.load('map.dump')
+
+
+if __name__ == '__main__':
+	m = Corridor()
+	m.get_map_from_database()
+	# m.create_corridor()
+
+	output = open('map.dump', 'wb')
+	pickle.dump(m, output)
+	output.close()
+
