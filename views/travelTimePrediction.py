38e6e6d56325a52a553e77c6edf5b77d3ae28eca
Yu-Chia Wu <gerrywu@utexas.edu>
2017-03-04 16:08:23 -0600

    pull travelsensor data

--- views/travelTimePrediction.py
+++ views/travelTimePrediction.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from database_setup import Base, User, TravelSensor, db_session, Summary
 
 travelTimePrediction = Blueprint('travelTimePrediction', __name__)
 
 @travelTimePrediction.route("/travelTimePrediction", methods=['GET'])
 def travelTimePrediction_function():
-    return render_template("travelTimePrediction.html")
+    travelSensors = db_session.query(TravelSensor).all()
+    return render_template("travelTimePrediction.html", travelSensors=travelSensors)
