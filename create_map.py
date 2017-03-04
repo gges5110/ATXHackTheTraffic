e044df2510eec5ab95d20467f9ce35a194dfc7e6
gy1122 <tiger.yang.g@gmail.com>
2017-03-04 15:32:48 -0600

    edit create_map

--- create_map.py
+++ create_map.py
@@ -9,7 +9,7 @@ class Map:
 		travelSensors = db_session.query(TravelSensor).all()
 		self.READER_IDs = [x.READER_ID for x in travelSensors]
 		for sensor in self.READER_IDs:
-			adjs = db_session.query(Summary.DESTINATION).filter(Summary.ORIGIN==sensor).distinct(Summary.DESTINATION).all()
+			adjs = db_session.query(Summary.Destination).filter(Summary.Origin==sensor).distinct(Summary.Destination).all()
 			self.ADJ_INTERSECTIONS[sensor] = [adj[0] for adj in adjs]
 			print sensor
 			print self.ADJ_INTERSECTIONS[sensor]
