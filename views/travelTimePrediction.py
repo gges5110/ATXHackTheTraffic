e800174189c8bdca1d9ec4c7e87d2690eeddd4ca
DavidTLiau <davidtliau@gmail.com>
2017-03-04 16:40:10 -0600

    Submit Travel

--- views/travelTimePrediction.py
+++ views/travelTimePrediction.py
@@ -3,7 +3,7 @@ from database_setup import Base, User, TravelSensor, db_session, Summary
 
 travelTimePrediction = Blueprint('travelTimePrediction', __name__)
 
-@travelTimePrediction.route("/travelTimePrediction", methods=['GET'])
+@travelTimePrediction.route("/travelTimePrediction", methods=['GET', 'POST'])
 def travelTimePrediction_function():
     travelSensors = db_session.query(TravelSensor).all()
     return render_template("travelTimePrediction.html", travelSensors=travelSensors)
