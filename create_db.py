4ca64aadfe48b785665b34ef91cc6a392155418a
Yu-Chia Wu <gerrywu@utexas.edu>
2017-03-05 16:30:23 -0600

    render map and process route request

--- create_db.py
+++ create_db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User
 
-engine = create_engine('sqlite:///database1.db')
+engine = create_engine('sqlite:///database.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
--- views/travelTimePrediction.py
+++ views/travelTimePrediction.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 from database_setup import Base, User, TravelSensor, db_session, Summary
-# from route import time_prediction
+from route import time_prediction
 
 travelTimePrediction = Blueprint('travelTimePrediction', __name__)
 
@@ -8,16 +8,25 @@ travelTimePrediction = Blueprint('travelTimePrediction', __name__)
 def travelTimePrediction_function():
 
     travelSensors = db_session.query(TravelSensor).all()
+    startTimeList = []
+    for i in range(0, 4*24):
+        if (i % 4) * 15 == 0:
+            startTimeList.append(str(i / 4) + ":00")
+        else:
+            startTimeList.append(str(i / 4) + ":" + str((i % 4) * 15))
+
+    # print startTimeList
 
     if request.method == 'POST':
-        time_pre = {
-        'route': [u'5th_trinity', u'7th_chicon', u'7th_plesant_valley', u'7th_springdale', u'7th_shady'],
-        'coord': [(30.2661324, -97.7396774), (30.2625771, -97.7221146), (30.2601261, -97.7094955), (30.2560368, -97.7016754), (30.2533531, -97.6975708)],
-        'time': 466        
-        }
-        # origin = request.form['origin']
-        # destination = request.form['destination']
-        # time_pre = time_prediction.findRoute(origin, destination, 0, 0)
-        return render_template("travelTimePrediction.html", travelSensors=travelSensors, time_pre=time_pre)
+        # time_pre = {
+        # 'route': [u'5th_trinity', u'7th_chicon', u'7th_plesant_valley', u'7th_springdale', u'7th_shady'],
+        # 'coord': [(30.2661324, -97.7396774), (30.2625771, -97.7221146), (30.2601261, -97.7094955), (30.2560368, -97.7016754), (30.2533531, -97.6975708)],
+        # 'time': 466
+        # }
+        origin = request.form['origin']
+        destination = request.form['destination']
+        minutes = (int(request.form['start_time']) - 1) * 15
+        time_pre = time_prediction.findRoute(origin, destination, 0, 0)
+        return render_template("travelTimePrediction.html", travelSensors=travelSensors, time_pre=time_pre, startTimeList=startTimeList)
     else:
-        return render_template("travelTimePrediction.html", travelSensors=travelSensors, time_pre=None)
+        return render_template("travelTimePrediction.html", travelSensors=travelSensors, time_pre=None, startTimeList=startTimeList)
