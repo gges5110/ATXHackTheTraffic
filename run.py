59d17455a3df4e26884514e73f375b2c21de8613
DavidTLiau <davidtliau@gmail.com>
2017-03-15 02:44:42 -0500

    HeatMap Created

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ from views.index import index
 from views.problemStatement import problemStatement
 from views.travelTimePrediction import travelTimePrediction
 from views.historyStats import historyStats
+from views.heatmap import heatmap
 from views.about import aboutUs
 
 app = Flask(__name__)
@@ -20,7 +21,9 @@ app.register_blueprint(index)
 app.register_blueprint(problemStatement)
 app.register_blueprint(travelTimePrediction)
 app.register_blueprint(historyStats)
+app.register_blueprint(heatmap)
 app.register_blueprint(aboutUs)
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
