b0edd52ffb56ae9a06412507f7aeb7ca2b243c33
DavidTLiau <davidtliau@gmail.com>
2017-03-23 03:16:55 -0500

    heatmap basic versionready

--- views/heatmap.py
+++ views/heatmap.py
@@ -16,10 +16,19 @@ def heatmap_function():
     year_selected = 2016
     weekday_selected = 0 # Monday is 0
     [Normalized_traveltime, Average_traveltime, Direction, Corridor_intersection] = corridor_traveltime(Corridor_name, year_selected, weekday_selected)
+    # Normalized_traveltime: contains Z-score of traveltime (drawing color base on this.) N[0]: N or E N[1]: S bound
+    # Average_traveltime: contains raw traveltime (drawing color base on this.) N[0]: N or E N[1]: S bound
+    # Direction: D[0]: "Northbound" or "Eastbound"
+    # Corridor_intersection: C[0] = ['lamar&1','lamar&2',...,'lamar&last']
     datamatrix=[]
+    datamatrix_second=[]
     for i in range(0, len(startTimeList)-1):
         for j in range(0,len(Corridor_intersection[0])-2):
             if Normalized_traveltime[0][j][i] <= 0.000000001:
                 Normalized_traveltime[0][j][i] = 0
             datamatrix.append(tuple([i,j,Normalized_traveltime[0][j][i]]))
-    return render_template("heatmap.html", startTimeList=startTimeList, traveltime_N=Normalized_traveltime , traveltime_bar=Average_traveltime, Direction=Direction, Corridor_intersection=Corridor_intersection, result=datamatrix)
+        for j in range(0,len(Corridor_intersection[1])-2):
+            if Normalized_traveltime[1][j][i] <= 0.000000001:
+                Normalized_traveltime[1][j][i] = 0
+            datamatrix_second.append(tuple([i,j,Normalized_traveltime[1][j][i]]))
+    return render_template("heatmap.html", startTimeList=startTimeList, traveltime_N=Normalized_traveltime , traveltime_bar=Average_traveltime, Direction=Direction, Corridor_intersection=Corridor_intersection, result=datamatrix, result2=datamatrix_second)
