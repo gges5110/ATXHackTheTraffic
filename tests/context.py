fe7a224324c24f8fe11b7d811e5ab235fcc4ea80
DavidTLiau <davidtliau@gmail.com>
2017-03-25 17:55:48 -0500

    delete test files

--- tests/context.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-
-# If the error occur: import error: 'No module named' does exist
-# use the command: export PYTHONPATH='.'
-from database_setup import Base, TravelSensor, Summary, db_session
